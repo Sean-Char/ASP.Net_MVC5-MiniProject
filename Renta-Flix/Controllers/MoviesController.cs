e8178e9a5cf9b9d0c81c7d3f019697f6457e763a
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-10 14:19:58 -0500

    MVC in Action

---
+++ Renta-Flix/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Renta_Flix.Models;
+
+namespace Renta_Flix.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+			var movie = new Movie() { Name = "The Predator" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ Renta-Flix/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Renta_Flix.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+}
