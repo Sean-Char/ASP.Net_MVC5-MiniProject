69c66a5de775fd9f5cb27e87e7a5f278c9a3c23b
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-10 20:38:44 -0500

    Action Parameters

--- Renta-Flix/Controllers/MoviesController.cs
+++ Renta-Flix/Controllers/MoviesController.cs
@@ -15,10 +15,23 @@ namespace Renta_Flix.Controllers
 			var movie = new Movie() { Name = "The Predator" };
 
 		    return View(movie);
-			//return Content("Hello World");
-			//return HttpNotFound();
-			//return new EmptyResult();
-			//return RedirectToAction("Index","Home", new { page = 1, sortBy = "name"});
         }
+
+		public ActionResult Edit(int id)
+		{
+			return Content("id=" + id);
+		}
+
+		// movies
+		public ActionResult Index(int? pageIndex, string sortBy)
+		{
+			if (pageIndex.HasValue)
+				pageIndex = 1;
+
+			if (string.IsNullOrWhiteSpace(sortBy))
+				sortBy = "Name";
+
+			return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+		}
     }
 }
