273949b5db7f0b4395f8dcd23aa6d4f0eebbfd51
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-11 12:52:52 -0500

    Querying Objects

--- Renta-Flix/Controllers/CustomersController.cs
+++ Renta-Flix/Controllers/CustomersController.cs
@@ -9,31 +9,33 @@ namespace Renta_Flix.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
-        public ActionResult Index()
+		private ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
+		public ActionResult Index()
         {
-			var customers = GetCustomer();
+			var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
-		public ActionResult Details(int id)
-		{
-			var customer = GetCustomer().SingleOrDefault(c => c.Id == id);
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-			if (customer == null)
-				return HttpNotFound();
+            if (customer == null)
+                return HttpNotFound();
 
-			return View(customer);
-		}
-
-		private IEnumerable<Customer> GetCustomer()
-		{
-			return new List<Customer>
-			{
-				new Customer { Id = 1, Name = "Dak Prescott" },
-				new Customer { Id = 2, Name = "Zeke Elliot" }
-			};
-		}
+            return View(customer);
+        }
     }
 }
