0642f3dbe33817f90e613e1bcd6409381393e59c
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-11 10:50:30 -0500

    Code-first Migrations

--- Renta-Flix/Models/IdentityModels.cs
+++ Renta-Flix/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Renta_Flix.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+		public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
