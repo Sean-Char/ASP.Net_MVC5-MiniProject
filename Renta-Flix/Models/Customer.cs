a6fc844427bf53ab9a091e17c443818f49e43f88
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-13 11:24:34 -0500

    Labels

--- Renta-Flix/Models/Customer.cs
+++ Renta-Flix/Models/Customer.cs
@@ -15,6 +15,8 @@ namespace Renta_Flix.Models
 		public bool IsSubscribedToNewsletter { get; set; }
 		public MembershipType MembershipType { get; set; }
 		public byte MembershipTypeId { get; set; }
+
+		[Display(Name = "Date of Birth")]
 		public DateTime? Birthdate { get; set; }
 	}
 }
