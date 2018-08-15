2d243dd5ed75b46e573576603b09b7d4515abb0d
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-15 13:07:05 -0500

    Adding Validation

--- Renta-Flix/Controllers/CustomersController.cs
+++ Renta-Flix/Controllers/CustomersController.cs
@@ -37,6 +37,17 @@ namespace Renta_Flix.Controllers
 		[HttpPost]
 		public ActionResult Save(Customer customer)
 		{
+			if (!ModelState.IsValid)
+			{
+				var viewModel = new CustomerFormViewModel
+				{
+					Customer = customer,
+					MembershipTypes = _context.MembershipType.ToList()
+				};
+
+				return View("CustomerForm", viewModel);
+			}
+
 			if (customer.Id == 0)
 				_context.Customers.Add(customer);
 			else
