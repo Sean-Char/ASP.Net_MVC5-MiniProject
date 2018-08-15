a33836142639e898b769f9c55f67625be5394d3b
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-15 14:34:28 -0500

    Custom Validation

--- Renta-Flix/Models/Customer.cs
+++ Renta-Flix/Models/Customer.cs
@@ -22,6 +22,7 @@ namespace Renta_Flix.Models
 		public byte MembershipTypeId { get; set; }
 
 		[Display(Name = "Date of Birth")]
+		[Min18YearsIfAMember]
 		public DateTime? Birthdate { get; set; }
 	}
 }
