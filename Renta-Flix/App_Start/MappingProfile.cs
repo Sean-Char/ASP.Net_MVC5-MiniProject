d894e3a3025d018e39f0ebeef29b5b838f60f06a
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-17 15:05:45 -0500

    Create DTOs for customer and installed Auto Mapper with Management console

---
+++ Renta-Flix/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿
+using AutoMapper;
+using Renta_Flix.Dtos;
+using Renta_Flix.Models;
+
+namespace Renta_Flix.App_Start
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			Mapper.CreateMap<Customer, CustomerDto>();
+			Mapper.CreateMap<CustomerDto, Customer>();
+		}
+	}
+	
+}
--- Renta-Flix/Global.asax.cs
+++ Renta-Flix/Global.asax.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Renta_Flix.App_Start;
 
 namespace Renta_Flix
 {
@@ -13,6 +11,7 @@ namespace Renta_Flix
     {
         protected void Application_Start()
         {
+			Mapper.Initialize(c => c.AddProfile<MappingProfile>());
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
