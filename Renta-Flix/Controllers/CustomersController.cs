da6c02d95a085a88711f018255ca036e735522cf
Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-28 14:37:24 -0500

    DataTables with Ajax Source

--- Renta-Flix/Controllers/CustomersController.cs
+++ Renta-Flix/Controllers/CustomersController.cs
@@ -69,9 +69,7 @@ namespace Renta_Flix.Controllers
 
 		public ViewResult Index()
         {
-			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
