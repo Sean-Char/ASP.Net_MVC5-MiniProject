876722455be7bb8e78c503b00ee54a419d76e4be
Sean Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-29 13:17:53 -0500

    Adding Profile Data

--- Renta-Flix/Models/AccountViewModels.cs
+++ Renta-Flix/Models/AccountViewModels.cs
@@ -63,7 +63,11 @@ namespace Renta_Flix.Models
     }
 
     public class RegisterViewModel
-    {
+    {	
+		[Required]
+		[Display(Name = "Driving License")]
+		public string DrivingLicense { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
