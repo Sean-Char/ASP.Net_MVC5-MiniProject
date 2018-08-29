3c50ced60b66fae7e20cb1335848f7be0625e1f9
Sean Charoutanh <Sean.Charoutanh@us.sogeti.com>
2018-08-29 10:40:16 -0500

    Restricting Access

--- Renta-Flix/App_Start/FilterConfig.cs
+++ Renta-Flix/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Renta_Flix
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
 			filters.Add(new HandleErrorAttribute());
+			filters.Add(new AuthorizeAttribute());
 		}
 	}
 }
--- Renta-Flix/Controllers/HomeController.cs
+++ Renta-Flix/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Renta_Flix.Controllers
 {
+	[AllowAnonymous]
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
